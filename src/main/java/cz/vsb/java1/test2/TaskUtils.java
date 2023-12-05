39157d6872da8f1826ce12a78f9afcc98dbe8181
janousek <janousek.jan@gmail.com>
2023-12-05 10:06:41 +0100

    utils

---
+++ src/main/java/cz/vsb/java1/test2/TaskUtils.java
@@ -0,0 +1,23 @@
+package cz.vsb.java1.test2;
+
+import cz.vsb.java1.test2.Vector3D;
+import cz.vsb.java1.test2.helpers.RandomGenarator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskUtils {
+
+    public static List<Vector3D> generateListOfVectors() {
+        return generateListOfVectors(10 + RandomGenarator.random.nextInt(10));
+    }
+
+    public static List<Vector3D> generateListOfVectors(int length) {
+        List<Vector3D> result = new ArrayList<>();
+        for (int i = 0; i < length; i++) {
+            result.add(Vector3D.generate());
+        }
+        return result;
+    }
+}
+
